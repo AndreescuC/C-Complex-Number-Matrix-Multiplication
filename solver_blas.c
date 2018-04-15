c0319268f461392911d8fd74e7c1c4fd0ecd7a63
AndreescuC <andreescuconstantin.ac95@gmail.com>
2018-04-15 10:48:30 +0300

    Added CBLAS source and libraries, implemented solver_blas

--- solver_blas.c
+++ solver_blas.c
@@ -4,11 +4,26 @@
  * Catalin Olaru / Vlad Spoiala
  */
 #include "utils.h"
+#include "CBLAS/include/cblas.h"
+#include "matrix_utils.h"
 
 /*
  * Add your BLAS implementation here
  */
 double* my_solver(int N, double *A) {
     printf("BLASS SOLVER\n");
-    return NULL;
+
+    enum CBLAS_ORDER order = CblasRowMajor;
+    enum CBLAS_UPLO uplo = CblasUpper;
+    enum CBLAS_TRANSPOSE transpose = CblasTrans;
+
+    double *C = malloc(N * N * 2 * sizeof(double));
+
+    int* one;
+    *one = 1;
+
+    cblas_zsyrk(order, uplo, transpose, N, N, &one, A, N, 0, C, N);
+    display_matrix(C, N);
+
+    return C;
 }
